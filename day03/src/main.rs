ab7919e6594fe140f9f256e7d2ca6bb69fd1b194
Tony Bradley <antbradley91@gmail.com>
2024-12-03 21:37:52 -0500

    day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,14 @@
 use core::str;
 use nom::IResult;
 
+fn do_parser(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    nom::bytes::complete::tag("do")(input)
+}
+
+fn dont_parser(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    nom::bytes::complete::tag("don't")(input)
+}
+
 fn mul_parser(input: &[u8]) -> IResult<&[u8], &[u8]> {
     nom::bytes::complete::tag("mul")(input)
 }
@@ -24,15 +32,77 @@ fn comma(input: &[u8]) -> IResult<&[u8], &[u8]> {
     nom::bytes::complete::tag(",")(input)
 }
 
-// parse mul(1,2)
-fn mul_expr(input: &[u8]) -> IResult<&[u8], (i32, i32)> {
+fn mul_expr(input: &[u8]) -> IResult<&[u8], Expr> {
     let (input, _) = mul_parser(input)?;
     let (input, _) = open_paren(input)?;
     let (input, a) = digit(input)?;
     let (input, _) = comma(input)?;
     let (input, b) = digit(input)?;
     let (input, _) = close_paren(input)?;
-    Ok((input, (a, b)))
+    Ok((input, Expr::Mul(a, b)))
+}
+
+fn do_expr(input: &[u8]) -> IResult<&[u8], Expr> {
+    let (input, _) = do_parser(input)?;
+    let (input, _) = open_paren(input)?;
+    let (input, _) = close_paren(input)?;
+    Ok((input, Expr::Do))
+}
+
+fn dont_expr(input: &[u8]) -> IResult<&[u8], Expr> {
+    let (input, _) = dont_parser(input)?;
+    let (input, _) = open_paren(input)?;
+    let (input, _) = close_paren(input)?;
+    Ok((input, Expr::Dont))
+}
+
+fn expr(input: &[u8]) -> IResult<&[u8], Expr> {
+    nom::branch::alt((do_expr, dont_expr, mul_expr))(input)
+}
+
+enum Expr {
+    Mul(i32, i32),
+    Do,
+    Dont,
+}
+
+fn part_two() -> i32 {
+    let mut input = &include_bytes!("input.txt")[..];
+    let mut result = 0;
+
+    let mut active: bool = true;
+
+    loop {
+        if input.is_empty() {
+            break;
+        }
+
+        match expr(input) {
+            Ok((i, Expr::Do)) => {
+                active = true;
+                input = i;
+            }
+            Ok((i, Expr::Dont))=> {
+                active = false;
+                input = i;
+            }
+            Ok((i, Expr::Mul(a, b))) => {
+                if active {
+                    result += a * b;
+                 }
+                 input = i;
+            }
+            Err(err)=> {
+                err.map(|e| {
+                    if !e.input.is_empty() {
+                        input = &e.input[1..]
+                    }
+                });
+            }
+        }
+    }
+
+    result
 }
 
 fn part_one() -> i32 {
@@ -45,8 +115,11 @@ fn part_one() -> i32 {
         }
 
         match mul_expr(input) {
-            Ok((i, (a, b))) => {
+            Ok((i, Expr::Mul(a, b))) => {
                 result += a * b;
+                input = i;
+            }
+            Ok((i, _)) => {
                 input = i;
             }
             Err(err) => {
@@ -56,7 +129,7 @@ fn part_one() -> i32 {
                     }
                     ()
                 });
-            },
+            }
         }
     }
 
@@ -65,4 +138,5 @@ fn part_one() -> i32 {
 
 fn main() {
     println!("Part one: {}", part_one());
+    println!("Part two: {}", part_two());
 }
